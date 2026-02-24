029befc424a93b1086cfb4b9e3a78e91d3359723
PhantomNota09 <srayala5@asu.edu>
2026-02-24 07:37:20 -0700

    Initial Commit

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Surya Rayala on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
